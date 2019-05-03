0bf88a9e8675bc89e8e0f3b1a82ea0c07d455c37
Yoan Ante <yoanante@gmail.com>
2019-05-02 22:16:22 -0400

    testing regex

--- logic.js
+++ logic.js
@@ -3,3 +3,12 @@ var gameWords = ["","","","",""];
 function randomWord(words){
     return words[Math.floor(Math.random() * Math.floor(words.length))]
 }
+
+// Watch for those that use regex
+function isCorrectGuess(word, letter){
+    return new RegExp(letter, "gi").test(word)
+}
+
+function getBlanks(word){
+   return word.replace(/[\s\S]/g, "_").split("");
+}
