7e656b79b1b314ad4ab7161bdd2f29e440ac5caa
Yoan Ante <yoanante@gmail.com>
2019-05-02 22:44:17 -0400

    started game logic

--- logic.js
+++ logic.js
@@ -12,3 +12,29 @@ function isCorrectGuess(word, letter){
 function getBlanks(word){
    return word.replace(/[\s\S]/g, "_").split("");
 }
+
+function fillBlanks(word, array, letter){
+    for (var i = 0; i < word.length ;i++){
+        if (word[i] === letter)  array[i] = letter
+    }
+    return array
+}
+
+function setupRound(word){
+    return {
+        "word": word,
+        "guessesLeft": 9,
+        "wrongGuesses": [],
+        "puzzleState": getBlanks(word)
+    }
+}
+
+function updateRound(theRound, guess){
+    if(isCorrectGuess(theRound.word, guess)){
+        theRound.guessesLeft--
+        theRound.puzzleState = fillBlanks(theRound.word, theRound.puzzleState, guess)
+    }else {
+        theRound.guessesLeft--;
+        theRound.guessesLeft.push(guess)
+    }
+}
